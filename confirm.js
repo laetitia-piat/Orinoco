a4114f3eeced57d05b8185a2cf0a79cdee485a38
Laetitia PIAT <laeptitchat29@gmail.com>
2020-09-14 15:10:54 +0200

    amelioration css

--- confirm.js
+++ confirm.js
@@ -5,15 +5,15 @@ console.log(total)
 
 
 const main = document.querySelector("main")
-main.className = 'container'
+main.className = 'container-fluid'
 const messageConfirm = document.createElement ("div")
 messageConfirm.className = 'text-center'
 const confirmation = document.createElement("h4")
 const commande = document.createElement("p")
-const numeroCommande = 202000000
+const numeroCommande = 2020+'TED'+'00000'
 
 confirmation.textContent = "Merci de votre commande d'un montant total de" + total + " EUROS" 
-commande.textContent = "Votre commande porte le numéro " + numeroCommande
+commande.textContent = "Votre commande porte le numéro  " + numeroCommande  + " À bientôt sur Orinoco.com"
 
 messageConfirm.appendChild(confirmation)
 messageConfirm.appendChild(commande)
